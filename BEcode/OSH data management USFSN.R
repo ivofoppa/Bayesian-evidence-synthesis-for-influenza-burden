d1c63cc8678417af71f190b39588aa5800ae58d7
ivofoppa <ivo.foppa@gmail.com>
2019-01-29 16:55:37 -0500

    Latest updates to data management

--- BEcode/OSH data management USFSN.R
+++ BEcode/OSH data management USFSN.R
@@ -55,12 +55,14 @@ ag <- sapply(oshdat$Age, function(ag) which(agels==ag))
 oshdat$ag <- ag
 
 oshdat$freq <- oshdat$COUNT
+oshdat$state <- oshdat$State
 
-oshdat <- oshdat[,c('seas','State','ag','osh','freq')]
+oshdat <- oshdat[,c('seas','state','ag','osh','freq')]
 #########################################################################################
 ### Age specific estimates from Millman et al. EID 2015, 21 (9):
 cipcrlist <- list(c(95.0,82,98.7),c(95.0,82,98.7),c(94.1,81.1,98.7),c(94.1,81.1,98.7),c(86.1,79.6,92.7))
 cirapidlist <- list(c(66.7,61.3,71.7),c(66.7,61.3,71.7),c(53.9,47.8,59.8),c(53.9,47.8,59.8),c(20.1,8.8,41.4))
+sensdata <- list(cipcrlist,cirapidlist)
 #########################################################################################
 ### Reading-in data set and making sure DateHosp is actually data variable  #############
 #########################################################################################
@@ -141,17 +143,92 @@ ag <- sapply(dataset$Age, function(ag) ifelse(ag %in% c(0:4),1,
                                                                    ifelse(ag %in% c(65:110),5,NA))))))
 dataset$ag <- ag
 dataset <- dataset[which(!is.na(ag)),]
+dataset$Died <- sapply(dataset$Died,function(d) ifelse(is.na(d),0,d))
 #########################################################################################
-### Aggregating dataset by season, state, ag, testing, test type and test result ########
+### Organizing dataset by season, state, ag, testing, test type and test result #########
 #########################################################################################
 # "State", "TestedFlu" ,"TestType","TestResult","TestType2","TestResult2","TestType3","TestResult3",
 #"Died","season","othtest","ag"
+dataset2 <- dataset[,c("State","season","ag", "TestedFlu" ,"TestType","TestResult","TestType2","TestResult2","TestType3","TestResult3",
+                       "Died")]
+TestType <- sapply(seq_along(dataset2$TestType), function(t) ifelse(dataset2$TestResult[t]==1,
+                                                                             dataset2$TestType[t],
+                                                                             ifelse(dataset2$TestResult2[t]==1,
+                                                                                    dataset2$TestType2[t],
+                                                                                    ifelse(dataset2$TestResult3[t]==1,
+                                                                                           dataset2$TestType3[t],
+                                                                                           dataset2$TestType[t]))))
 
+TestResult <- sapply(seq_along(dataset2$TestResult), function(t) ifelse(dataset2$TestResult[t]==1,
+                                                                             dataset2$TestResult[t],
+                                                                             ifelse(dataset2$TestResult2[t]==1,
+                                                                                    dataset2$TestResult2[t],
+                                                                                    ifelse(dataset2$TestResult3[t]==1,
+                                                                                           dataset2$TestResult3[t],
+                                                                                           dataset2$TestResult[t]))))
+TestType <- sapply(TestType,function(tt) ifelse(tt==1,1,ifelse(tt==2,2,ifelse(tt>2,3,0))))
+dataset2$TestType <- TestType
+dataset2$TestResult <- TestResult
+
+dataset2 <- dataset[,c("State","season","ag", "TestedFlu" ,"TestType","TestResult","Died")]
+
+dataset2$TestedFlu[which(is.na(dataset2$TestedFlu) | dataset2$TestedFlu==2)] <- 0
+dataset2$TestedFlu[which(dataset2$TestResult==1)] <- 1
+
+dataset2$TestResult[which(is.na(dataset2$TestResult) | dataset2$TestResult==2)] <- 0
+#########################################################################################
+### Aggregating dataset by season, state, ag, testing, test type and test result ########
+#########################################################################################
+State <- dataset2$State
+seas <- dataset2$seas
+ag <- dataset2$ag
+TestedFlu <- dataset2$TestedFlu
+TestedType <- dataset2$TestedType
+TestResult <- dataset2$TestResult
+Died <- dataset2$Died
+
+agdataset <- NULL
+
+for (st in statels){
+  for (s in 1:6){
+    for (a in 1:5){
+      for (died in 0:1){
+        for (t in 0:1){
+          if (t==1){
+            for (tt in 1:3){
+              for (res in 0:1){
+                selind <- which(State==st & seas==s & ag==a & Died==died & TestedFlu==1 & TestType==tt & TestResult==res)
+                row <- c(st,s,a,died,t,tt,res,length(selind))
+              }
+            }
+          } else {
+            selind <- which(State==st & seas==s & ag==a & Died==died & TestedFlu==0)
+            row <- c(st,s,a,died,0,NA,NA,length(selind))
+          }
+          agdataset <- rbind(agdataset,row,deparse.level = 0)
+        }
+      }
+      
+    }
+  }
+}
+
+for (col in 2:length(agdataset[1,])){
+  agdataset[,col] <- as.numeric(agdataset[,col])
+}
+
+agdataset <- data.frame(agdataset)
+
+colnames(agdataset) <- c("State","season","ag", "TestedFlu" ,"TestType","TestResult","Died","freq")
+FluSurvdata <- agdataset
 #########################################################################################
 ###  Reading-in denominator data ########################################################
 #########################################################################################
 library(readxl)
-# 
+#,
+statels2 <- c("CA","GA","OR","CT","CO","NM","NYA","NYR","MN","TN","MI")
+
+setwd(paste0(bfolder,'BEdata'))
 agseaspop <- NULL
 
 for (s in 1:7){
@@ -159,7 +236,7 @@ for (s in 1:7){
   xlsFile <- paste0('NCHS ',seas,' population estimates.xls')
   popfile <- read_excel(xlsFile, sheet = 1)
   for (st in statels){
-    stpopselind <- which(substr(stnms,1,2)==st)+1
+    stpopselind <- which(substr(statels2,1,2)==st)+1
     for (ag in 1:5){
       aglim <- agecatlist[[ag]][[1]]
       drow <- c(s,ag,st,sum(popfile[aglim,stpopselind]))
@@ -167,12 +244,15 @@ for (s in 1:7){
     }
   }
 }
-colnames(agseaspop) <- c('season','ag','state','pop')
+colnames(agseaspop) <- c('seas','ag','state','pop')
 agseaspop <- data.frame(agseaspop)
+agseaspop <- agseaspop[,c('state','seas','ag','pop')]
 #########################################################################################
 setwd(paste0(bfolder,'BEdata'))
-outfname <- 'OSHdeath_2010-2016,FluSURV-NET.csv'
-write.csv(dataset2cum,file = outfname,quote = F)
+outfname <- 'FluSURV-NET_burden.RData'
+fulldata <- list(FluSurvdata=FluSurvdata,sensdata=sensdata,agseaspop=agseaspop,oshdat=oshdat)
+
+save(fulldata,file = outfname)
 #########################################################################################
 #########################################################################################
 #########################################################################################
