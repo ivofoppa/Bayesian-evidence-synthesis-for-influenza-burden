2022f9af66090b554f2b2127c43df1c06f07acbe
ivofoppa <ivo.foppa@gmail.com>
2019-02-27 11:18:36 -0500

    Update test.cpp

--- BEcode/Rccp/test.cpp
+++ BEcode/Rccp/test.cpp
@@ -13,25 +13,15 @@ using namespace Rcpp;
 //
 
 // [[Rcpp::export]]
-int choosecpp(int n, int k)
-{
-  { 
-    int res = 1; 
-    
-    // Since C(n, k) = C(n, n-k) 
-    if ( k > n - k ) 
-      k = n - k; 
-    
-    // Calculate value of [n * (n-1) *---* (n-k+1)] / [k * (k-1) *----* 1] 
-    for (int i = 0; i < k; i++) 
-    { 
-      res *= (n - i); 
-      res /= (i + 1); 
-    } 
-    
-    return res; 
-  } 
-}
+int choosecpp(int n, int k) { 
+  // Base Cases 
+  if (k==0 || k==n) 
+    return 1; 
+  if ( k > (n - k))
+    k = n - k ;
+  // Recur 
+  return  choosecpp(n-1, k-1) + choosecpp(n-1, k); 
+} 
 // [[Rcpp::export]]
 double binompdf(int x, int n, double p) 
 {
