d4336a0f78cf9552f2affbdf7c6585f2ebd8428b
ivofoppa <ivo.foppa@gmail.com>
2019-03-09 14:18:04 -0500

    Update binom_slice.cpp

--- BEcode/Rccp/binom_slice.cpp
+++ BEcode/Rccp/binom_slice.cpp
@@ -1,11 +1,19 @@
 #include <Rcpp.h>
-
 using namespace Rcpp;
 
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
 // [[Rcpp::export]]
 NumericVector binomslicep(int x, int n, double p0, double delta, int num ) 
 {
-  double f0, f1, f2, fnew, yran, p1, p2, pran ;
+  double f0, f1, f2, fnew, p1, p2, pran, z ;
   NumericVector pout(num) ;
   
   if ( (p0 < 0) || (p0 > 1) || (x > n)) 
@@ -17,23 +25,23 @@ NumericVector binomslicep(int x, int n, double p0, double delta, int num )
     pran = p0 ;
     for ( int i=0 ; i <= num ; i++ ) {
       f0 = R::dbinom(x, n, pran, false) ;
-      yran = R::runif(0,f0) ;
-      p1 = std::max(p0 - delta, 0.) ;
-      p2 = std::min(p0 + delta, 1.) ;
+      z = R::runif(0,f0) ;
+      p1 = std::max(pran - delta, 0.) ;
+      p2 = std::min(pran + delta, 1.) ;
       
       f1 = R::dbinom(x, n, p1, false) ;
       f2 = R::dbinom(x, n, p2, false) ;
       
-      while ( f1 > yran && p1 > 0 ) {
+      while ( f1 > z && p1 > 0 ) {
         p1 = std::max(p1 - delta, 0.) ;
         f1 = R::dbinom(x, n, p1, false) ;
       }
-      while ( f2 > yran && p2 < 1) {
+      while ( f2 > z && p2 < 1) {
         p2 = std::min(p2 + delta, 1.) ;
         f2 = R::dbinom(x, n, p2, false) ;
       }
-      fnew = 0.;
-      while ( fnew  < yran ) {
+      fnew = 0;
+      while ( fnew  < z ) {
         pran = R::runif(p1,p2) ;
         fnew = R::dbinom(x, n, pran, false) ;
       }
@@ -42,47 +50,8 @@ NumericVector binomslicep(int x, int n, double p0, double delta, int num )
     return pout ;
   }
 }
-// [[Rcpp::export]]
-IntegerVector binomslicen(int x, int n0, double p, int delta, int num ) 
-{
-  double f0, f1, f2, fnew, yran, numran ;
-  int n1, n2, nran ;
-  IntegerVector nout(num) ;
-  
-  if ( (p < 0) || (p > 1) || (n0 < 1) || (x < 0) || (n0 < x)) 
-  {
-    return(0) ; 
-  }
-  else 
-  {
-    nran = n0 ;
-    for (int i=0 ; i <= num ; i++ ) {
-      f0 = R::dbinom(x, nran, p, false) ;
-      yran = R::runif(0,f0) ;
-      n1 = std::max(nran - delta, 0) ;
-      n2 = nran + delta ;
-      
-      f1 = R::dbinom(x, n1, p, false) ;
-      f2 = R::dbinom(x, n2, p, false) ;
-      
-      while ( f1 > yran && (n1 - delta) >= x ) {
-        n1 = std::max(n1 - delta, x) ;
-        f1 = R::dbinom(x, n1, p, false) ;
-      }
-      n1 = std::max(n1, x) ;
-      while ( f2 > yran ) {
-        n2 = n2 + delta ;
-        f2 = R::dbinom(x, n2, p, false) ;
-      }
-      fnew = 0.;
-      while ( fnew < yran ) {
-        numran = R::runif(0,1) ;
-        numran = numran * (n2 - n1) + n1 ;
-        nran = round(numran) ;
-        fnew = R::dbinom(x, nran, p, false) ;
-      }
-      nout[i] = nran ;
-    }
-    return(nout) ;
-  }
-}
+
+
+/*** R
+binomslicep(20,100,.3,.1,100)
+*/
