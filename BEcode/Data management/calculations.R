eb33a5e6f8119db43641edf9aaf00ee01e9f63a0
ivofoppa <40694381+ivofoppa@users.noreply.github.com>
2020-03-06 10:56:36 -0500

    delete old folders

--- BEcode/Data management/calculations.R
+++
@@ -1,8 +0,0 @@
-sapply(1:8,function(s) 1 - nttypearr[1,4,s]/sum(nttypearr[1,,s]))
-sapply(1:8,function(s) 1 - nttypearr[2,4,s]/sum(nttypearr[2,,s]))
-
-sapply(1:8,function(s) 1 - sum(nttypearr[,4,s])/sum(nttypearr[,,s]))
-
-selindst <- which(dataset$TestedFlu==1)
-dataset2 <- dataset[selindst,]
-unique(dataset$State)
--- BEcode/obsolete/beta_fit.R
+++
@@ -1,9 +0,0 @@
-mu <- 0.6
-sd <- .1
-
-fit_beta <- function(mu,sd){
-  beta0 <- 1
-  alpha0 <- mu*beta0/(1 - mu)
-  var0 <- sqrt(alpha0*beta0/((alpha0 + beta0)^2 * (alpha0 + beta0 + 1)))
-}
-
